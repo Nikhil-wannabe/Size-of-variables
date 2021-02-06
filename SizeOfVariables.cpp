f90e740f41a70bd1ae37b661043eda77b401866b
Nikhil-wannabe <71582098+Nikhil-wannabe@users.noreply.github.com>
2021-02-06 13:14:42 +0530

    Create SizeOfVariables.cpp

---
+++ SizeOfVariables.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+#include<conio.h>
+#include <iostream>
+ int main(){
+   cout<<"The size of int is: " << sizeof(int) <<"\n" ;
+   cout<<"The size of float is: " << sizeof(float) <<"\n" ;
+   cout<<"The size of double is: " << sizeof(double) <<"\n" ;
+   cout<<"The size of char is: " << sizeof(char) <<"\n" ;
+
+  
+ }
